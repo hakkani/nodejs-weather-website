df39a3ffeb78f23cec6105febf05b6b92641704e
Hakkani <hakkani@linkstaff.co.jp>
2020-04-15 17:58:06 +0900

    Updated forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,6 +3,8 @@ const request = require("request");
 const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=0145e67e212be7422c4c29669f4ef7be&query=${latitude},${longitude}&units=f`;
   request({ url, json: true }, (error, { body }) => {
+    console.log(body);
+
     if (error) {
       callback("Unable to connect to weather services!", undefined);
     } else if (body.error) {
@@ -10,7 +12,7 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        `${body.current.weather_descriptions[0]}. It is currently Tempareture: ${body.current.temparature} degree and Humidity: ${body.current.humidity}. It feel likes ${body.current.feelslike} degree out.`
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degree out. It feel likes ${body.current.feelslike} degree out. The humidity is ${body.current.humidity}%`
       );
     }
   });
